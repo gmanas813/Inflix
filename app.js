df8f6eafd41ba7ad1253d9f5aeee4222bf700cbf
Ubuntu <ubuntu@ip-172-31-80-9.ec2.internal>
2020-11-23 07:21:48 +0000

    final

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+
 var express=require("express");
 
 var mongoose=require("mongoose");
 var reqs=require("request");
-//mongoose.connect("mongodb://localhost/rockershocks5");
-mongoose.connect("mongodb+srv://rocko:rockalways@rockershock-ptdgc.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb://localhost/rockershocks5");
+//mongoose.connect("mongodb+srv://rocko:rockalways@rockershock-ptdgc.mongodb.net/test?retryWrites=true&w=majority");
 //mongoose.connect("mongodb+srv://rocko:rockalways@rockershock-ptdgc.mongodb.net/test?retryWrites=true&w=majority");
 var methodoverride=require("method-override");
 var flash=require("connect-flash");
@@ -43,6 +44,9 @@ app.use(function(req, res, next){
      res.render("root.ejs");
  });
  
+ const sgMail=require('@sendgrid/mail');
+sgMail.setApiKey("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");
+const crypto=require('crypto');
  
  app.get("/profiles",function(req,res){
   Profile.find({},function(err,user){
@@ -124,25 +128,84 @@ app.put("/profiles/:id",ifloggedin,function(req,res){
        res.render("user/register.ejs");
    });
    
-   app.post("/register",function(req,res){
-  var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
+   app.post("/register",async function(req,res){
+  var newUser = new User({
+   username: req.body.username,
+   email:req.body.email,
+   emailToken: crypto.randomBytes(64).toString('hex'),
+   isVerified: false
+  });
+    User.register(newUser, req.body.password,async function(err, user){
         if(err){
             console.log(err);
             return res.render("user/register.ejs");
         }
-        passport.authenticate("local")(req, res, function(){
-         req.flash("success","Successfully Registered!");
-           res.redirect("/"); 
-        });
+        // passport.authenticate("local")(req, res, function(){
+        //  req.flash("success","Successfully Registered!");
+        //    res.redirect("/"); 
+        // });
+      const msg={
+       from:'manasgupta318@gmail.com',
+       to:user.email,
+       subject:'Verification',
+       text:`
+       verify http://${req.headers.host}/verify-email?token=${user.emailToken}
+       `,
+     	html: `
+                 <h1>Hello, </h1>
+                 <p>Thanks for registering on our site Inflix. </p>
+                 <p>Please Click the link below to verify your account. </p>
+                 <a href="https://${req.headers.host}/verify-email?token=${user.emailToken}">Verify Your Account </a>
+            `
+      }
+			
+		
+		try{
+			await sgMail.send(msg);
+			req.flash('success', 'Thanks for registering. Please check your email to verify your account.')
+			res.redirect('/');
+		}catch(error){
+			console.log(error);
+			req.flash('error',' Something went wrong. Please contact manasgupta318@gmail.com');
+			
+		}
+       
+        
     });
    });
+   
+   app.get('/verify-email',async(req,res,next)=>{
+	try{
+		const user=await User.findOne({emailToken:req.query.token});
+		if(!user){
+			req.flash('error','Token is invalid. Please contact the admin');
+			return res.redirect('/');
+			
+		}
+		user.emailToken=null;
+		user.isVerified=true;
+		await user.save();
+		await req.login(user,async(err)=>{
+			if(err) return next(err);
+			req.flash('success',`Welcome to Inflix ${user.username}`);
+			const redirectUrl='/profiles';
+			delete req.session.redirectTo;
+			res.redirect(redirectUrl);
+			
+		});
+	}catch(error){
+		console.log(error);
+		req.flash('error','Token is invalid. Please contact the admin');
+		res.redirect('/');
+		
+	}
+});
    
    app.get("/login",function(req, res) {
        res.render("user/login.ejs");
    });
    
-   app.post("/login",passport.authenticate("local",{
+   app.post("/login",isNotVerified,passport.authenticate("local",{
     successRedirect : "/",
     failureRedirect : "/login"
    }),function(req,res){
@@ -306,8 +369,30 @@ Profile.findById(req.params.id,function(err,found){
  req.flash("error","Please Login First");
  res.redirect("/login");
 }
-  
+ async function isNotVerified (req,res,next){
+  try{
+        const user=await User.findOne({username:req.body.username});
+
+
+        if(user.isVerified){
+            return next();
+        }
+        else{
+            req.flash('error','Your Account is not verified. Please check your email and verify your account ');
+            return res.redirect('/');
+
+        }
+
+    }catch(error){
+        console.log(error);
+        req.flash('error','Something Went Wrong. Please contact the admin');
+        res.redirect('/')
+
+
+    }
+}
    
  app.listen(process.env.PORT,process.env.IP,function(){
   console.log("started");
  });
+ 
--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ var mongoose=require("mongoose");
 var passportLocalMongoose=require("passport-local-mongoose");
 var userschema=new mongoose.Schema({
     username:String,
-    password:String
+    password:String,
+    email:String,
+    emailToken:String,
+    isVerified:Boolean
 });
 userschema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("User",userschema);
